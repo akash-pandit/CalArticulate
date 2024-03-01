2aa13aeb4dd01535283a656d97cfc99d8bded4a7
akash-pandit <34327539+akash-pandit@users.noreply.github.com>
2024-02-29 22:10:08 -0800

    continued test.js

--- data/test.js
+++ data/test.js
@@ -1,5 +1,5 @@
 import puppeteer from "puppeteer";
-import urlsBy4yr from './assist_urls.json';
+import urlsBy4yr from './assist_urls.json';  // '4yr' == 'state school'
 
 /**
  * async func that scrapes course data between 1 state school and 1 cc w/ Puppeteer
@@ -25,10 +25,12 @@ async function scrapeURL(browser, url) {
 }
 
 /**
- * 
+ * helper function for scrapeURL
+ * async func that grabs and cleans the actual text from the given css element
+ * on a given puppeteer page object & returns it
  * @param {String} cssSelector
  * @param {puppeteer.Page} page 
- * @returns {Promise<String[]>} 
+ * @returns {Promise<String[]>} an array of cleaned courses + descriptions
  */
 async function getCourses(cssSelector, page) {
     await page.$$eval('.rowReceiving', element => element.map(element => {
@@ -37,3 +39,10 @@ async function getCourses(cssSelector, page) {
         // TODO: add logic for cleaning course data
     }));
 }
+
+// "main" function
+(async () => {
+    Object.keys(urlsBy4yr).forEach(stateID => {
+        console.log(stateID);
+    });
+});
